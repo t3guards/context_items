4f99c21fef584fab4c4968bfd76cee70d4575d65
Rene Gast <shake-it@cogtail.de>
2020-11-30 15:13:20 +0100

    Change extension infos

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 /***************************************************************
- * Extension Manager/Repository config file for ext "examples".
+ * Extension Manager/Repository config file for ext "context_items".
  *
- * Auto generated 28-11-2020 15:48
+ * Auto generated 30-11-2020 15:08
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -11,29 +11,28 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-  'title' => 'Core Documentation Code Examples',
-  'description' => 'This extension packages a number of code examples from the Core Documentation.',
-  'category' => 'example',
-  'author' => 'Documentation Team',
-  'author_email' => 'documentation@typo3.org',
-  'state' => 'alpha',
+  'title' => 'Context Items',
+  'description' => 'Items for context menu.',
+  'category' => 'backend',
+  'author' => 'T3 Guards',
+  'author_email' => 'kontakt@t3guards.de',
+  'state' => 'beta',
   'uploadfolder' => false,
   'clearCacheOnLoad' => 0,
   'author_company' => '',
   'version' => '10.0.1',
-  'constraints' => 
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
       'typo3' => '10.4.10-10.4.99',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
   'clearcacheonload' => false,
 );
-
